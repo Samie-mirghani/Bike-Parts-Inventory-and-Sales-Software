af9fc9b99bf3b90e2c0cd7d2bd41403b9d132e20
shamsadeanmirghani <shamsadeanmirghani@10.213.177.145>
2017-11-27 17:58:40 -0500

    Passwords are all working. Look at the PasswordController.java file to find all acceptable passwords.

--- src/project/pkg3/Project3.java
+++ src/project/pkg3/Project3.java
@@ -1,7 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * This class initializes the GUI interface and the rest of the project
+ * @author shamsadeanmirghani
  */
 package project.pkg3;
 
@@ -11,10 +10,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- *
- * @author shamsadeanmirghani
- */
+
 public class Project3 extends Application {
     
     @Override
